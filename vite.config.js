ac8b6874d8602d9b272d666eeb531faad6333895
sean-benedict-bernardo <145823446+sean-benedict-bernardo@users.noreply.github.com>
2023-11-19 13:35:35 +0800

    Update vite.config.js
    Changed vite.config.js for change from Github Pages to Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/UXSTaft2023-DevDeptApp-BERNARDO/',
+  base: './',
   plugins: [
     vue(),
   ],
